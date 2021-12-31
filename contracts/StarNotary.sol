0bf02c4150b7b5c9f625a582be4d11011b4eadb8
littbarskiadeh <lit.adeh@gmail.com>
2021-12-31 09:52:08 -0500

    added tests for Smart Contract. 5/5 tests passing

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.24;
+pragma solidity ^0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-contract StarNotary is ERC721 ("StarNotary","SN") {
+contract StarNotary is ERC721  {
 
     struct Star {
         string name;
@@ -13,39 +13,44 @@ contract StarNotary is ERC721 ("StarNotary","SN") {
     mapping(uint256 => Star) public tokenIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
 
+    constructor() ERC721("StarNotary","SN") {    }
+
     
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public {
         Star memory newStar = Star(_name);
         tokenIdToStarInfo[_tokenId] = newStar;
         _mint(msg.sender, _tokenId);
+        // setApprovalForAll(address(this), true);
     }
 
     // Putting an Star for sale (Adding the star tokenid into the mapping starsForSale, first verify that the sender is the owner)
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
-        require(ownerOf(_tokenId) == msg.sender, "You can't sale the Star you don't owned");
+        require(ownerOf(_tokenId) == msg.sender, "You can't sell the Star you don't owned");
         starsForSale[_tokenId] = _price;
     }
 
-    function _make_payable(address x) internal pure returns (address payable) {
-           address payable payableAddress = payable(x);
-        // return address(uint160(x));
-        return payableAddress;
-    }
+    // function _make_payable(address x) internal pure returns (address payable) {
+    //     //    address payable payableAddress = payable(x);
+    //     return address(uint160(x));
+    //     // return payableAddress;
+    // }
 
     function buyStar(uint256 _tokenId) public  payable {
         require(starsForSale[_tokenId] > 0, "The Star should be up for sale");
         uint256 starCost = starsForSale[_tokenId];
         address ownerAddress = ownerOf(_tokenId);
         require(msg.value > starCost, "You need to have enough Ether");
-        safeTransferFrom(ownerAddress, _make_payable(msg.sender), _tokenId,'');
         // approve(msg.sender, _tokenId); // added for test
-        address payable ownerAddressPayable = _make_payable(ownerAddress);
+        transferFrom(ownerAddress, msg.sender, _tokenId);
+        address payable ownerAddressPayable = payable(ownerAddress);
         ownerAddressPayable.transfer(starCost);
 
         if(msg.value > starCost) {
             // ownerAddressPayable.transfer(starCost);
-            _make_payable(msg.sender).transfer(msg.value - starCost);
+            address payable callerAddressPayable = payable(msg.sender);
+
+            callerAddressPayable.transfer(msg.value - starCost);
         }
     }
 
--- test/StarNotary.test.js
+++ test/StarNotary.test.js
@@ -2,26 +2,26 @@ const StarNotary = artifacts.require("StarNotary");
 
 var accounts;
 var owner;
-// let instance ;
+let instance ;
 
 contract('StarNotary', async(accs) => {
     accounts = accs;
     owner = accounts[0];
-    // instance = await StarNotary.deployed();
 });
 
-// beforeEach(async() => {
-// })
+beforeEach(async () => {
+    instance = await StarNotary.deployed();
+  });
 
 it('can Create a Star', async() => {
     let tokenId = 1;
-    let instance = await StarNotary.deployed();
+    // let instance = await StarNotary.deployed();
     await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]})
     assert.equal(await instance.tokenIdToStarInfo.call(tokenId), 'Awesome Star!')
 });
 
 it('lets user1 put up their star for sale', async() => {
-    let instance = await StarNotary.deployed();
+    // let instance = await StarNotary.deployed();
     let user1 = accounts[1];
     let starId = 2;
     let starPrice = web3.utils.toWei(".01", "ether");
@@ -30,8 +30,8 @@ it('lets user1 put up their star for sale', async() => {
     assert.equal(await instance.starsForSale.call(starId), starPrice);
 });
 
-it('lets user1 get the funds after the sale', async() => {
-    let instance = await StarNotary.deployed();
+it('lets user1 get the funds after the sale', async () => {
+    // let instance = await StarNotary.deployed();
     let user1 = accounts[1];
     let user2 = accounts[2];
     let starId = 3;
@@ -40,15 +40,23 @@ it('lets user1 get the funds after the sale', async() => {
     await instance.createStar('awesome star', starId, {from: user1});
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
     let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user1);
-    await instance.buyStar(starId, {from: user2, value: balance});
+
+    //`user2` is not the token owner so it has to be approved.
+    await instance.approve(user2, starId, {from: user1, gasPrice: 0});  // buyStar() requires that the caller is the owner or approved to transfer that token. 
+    await instance.buyStar(starId, {from: user2, value: balance, gasPrice: 0});
     let balanceOfUser1AfterTransaction = await web3.eth.getBalance(user1);
+    // Check that the funds were transmitted
     let value1 = Number(balanceOfUser1BeforeTransaction) + Number(starPrice);
     let value2 = Number(balanceOfUser1AfterTransaction);
+    
+    // console.log(balanceOfUser1BeforeTransaction, starPrice)
+    // console.log(value1, value2);
+
     assert.equal(value1, value2);
 });
 
 it('lets user2 buy a star, if it is put up for sale', async() => {
-    let instance = await StarNotary.deployed();
+    // let instance = await StarNotary.deployed();
     let user1 = accounts[1];
     let user2 = accounts[2];
     let starId = 4;
@@ -57,12 +65,15 @@ it('lets user2 buy a star, if it is put up for sale', async() => {
     await instance.createStar('awesome star', starId, {from: user1});
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
     let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
+
+    await instance.approve(user2, starId, {from: user1, gasPrice: 0});
     await instance.buyStar(starId, {from: user2, value: balance});
+
     assert.equal(await instance.ownerOf.call(starId), user2);
 });
 
 it('lets user2 buy a star and decreases its balance in ether', async() => {
-    let instance = await StarNotary.deployed();
+    // let instance = await StarNotary.deployed();
     let user1 = accounts[1];
     let user2 = accounts[2];
     let starId = 5;
@@ -72,6 +83,8 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
     let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
     const balanceOfUser2BeforeTransaction = await web3.eth.getBalance(user2);
+
+    await instance.approve(user2, starId, {from: user1, gasPrice: 0});
     await instance.buyStar(starId, {from: user2, value: balance, gasPrice:0});
     const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
     let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
